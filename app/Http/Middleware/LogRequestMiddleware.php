4b743b55d14c13a735451809ea9bf234bbe3dd4d
Dwain <dwaincontact@gmail.com>
2026-01-14 11:01:03 +0100

    exo 5

---
+++ app/Http/Middleware/LogRequestMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogRequestMiddleware
+{
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        $userAgent = $request->header('User-Agent');
+
+        Log::info('Requête reçue', [
+            'user_agent' => $userAgent
+        ]);
+
+        return $next($request);
+    }
+}
